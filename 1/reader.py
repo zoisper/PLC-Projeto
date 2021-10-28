1117598d9c7e621af99b5a11cbb92baf394a5fe1
sorrybro55 <62657679+sorrybro55@users.noreply.github.com>
2021-10-28 12:41:06 +0100

    Add files via upload

--- 1/reader.py
+++ 1/reader.py
@@ -2,16 +2,19 @@ import re
 
 with open('/home/gomes/Desktop/PLC/Trabalho/clav-users.txt', 'r') as f: 
     inputline_raw = f.readlines()
-    name_list = {}
-    entity_list = {}
+    list = []
 
     for line in inputline_raw:
         print(line)
         print()
-        param_line = re.split(r':: ',line)
-        (name,entidade) = re.search(r'^([A-Z][a-z]+\s)((([A-Z][a-z]+|(de)|(da)|(do)))\s)+', line),re.search(r'ent_[A-Za-z0-9]+(-\w+)?',line)
-        print((name,entidade))
-        print()
+        param_line = re.split(r': ',line)
+        (name,entity) = re.search(r'^([A-Z][a-z]+\s)((([A-Z][a-z]+|(de)|(da)|(do)))\s)+:', line),re.search(r'ent_[A-Za-z0-9]+(-\w+)?',line)
+        
+        if name != None and entity != None:
+            list.append((name.group()[:-2],entity.group()))
+    
+    for x in list:
+        print(x)
 
 
 #entidades = re.findall(r'ent_[A-Za-z0-9]+(-\w+)?', inputline_raw)
