64bc9a30974d2d6f15b7aacd5eb492825e531024
zoisper <zoisper@gmail.com>
2021-10-30 23:35:26 +0100

    wip

--- 1/main.py
+++ 1/main.py
@@ -1,6 +1,7 @@
 import re
 import unidecode
 
+
 '''Construa agora um ou vários programas
 para processar o texto ' clav-users.txt ' em que campos de informação
 têm a seguinte ordem: nome, email, entidade, nível, número de chamadas ao backend, com o intuito de calcular alguns
@@ -29,15 +30,15 @@ def name_entity_list():
 #utilizadores estão registados;
 
 def entity_num_elements_list():
-    result = {}
+    entities = {}
     for line in text:
         entity = re.search(r'ent_\w*', line)
-        if entity.group() not in result:
-            result[entity.group()] = 1
+        if entity.group() not in entities:
+            entities[entity.group()] = 1
         else:
-            result[entity.group()] +=1
+            entities[entity.group()] +=1
 
-    result = list(result.items())
+    result = list(entities.items())
     result.sort()
     print("*** Entidades - Nº Utilizadores  ***")
     for r in result:
@@ -84,16 +85,94 @@ def name_entity_group():
             print("*",user)
         print("")
 
+#Por fim, produz os seguintes indicadores:
+
+#1. Quantos utilizadores?
+#2. Quantas entidades?
+#3. Qual a distribuição em número por entidade?
+#4. Qual a distribuição em número por nível?
+
+def indicators():
+    users = set()
+    entities = {}
+    levels = {}
+    for line in text:
+        broken_line = re.split(r'::', line)
+        users.add(re.match(r'(\w+\s*(\w+\s*)*\b)',broken_line[0]).group())
+        entity = re.search(r'ent_\w*', broken_line[2]).group()
+        level = re.search(r'\d+\.?\d*', broken_line[3]).group()
+
+        if entity not in entities:
+            entities[entity] = 1
+        else:
+            entities[entity] += 1
+        if level not in levels:
+            levels[level] = 1
+        else:
+            levels[level] += 1
+
+    print("*** Indicadores ***")
+    print(len(users), "Utilizadores")
+    print(len(entities.keys()), "Entidades")
+    print("")
+
+    print("Distribuição de utilizadores por entidade:")
+    for entity in sorted(entities.keys()):
+        print(f"* {entity} - {entities[entity]}")
+    print("")
+    print("Distribuição de utilizadores por nivel:")
+    for level in sorted(levels.keys()):
+        print(f"* {level} - {levels[level]}")
+
+#Para terminar, deve imprimir os 20 primeiros registos num novo cheiro de output mas em formato.
+
+def json_20():
+    return 0
 
 
 
 
 
+def menu():
+    cls = lambda: print('\n' * 50)
+    inputfromuser = ""
+    options = []
+    options.append("Listagem com o nome e a entidade do utilizador, ordenada alfabeticamente por nome.")
+    options.append("Lista ordenada alfabeticamente das entidades referenciadas, indicando, para cada uma, quantos utilizadores estão registados.")
+    options.append("Distribuição de utilizadores por níveis de acesso.")
+    options.append("Utilizadores, agrupados por entidade, ordenada primeiro pela entidade e dentro desta pelo nome.")
+    options.append("Mostrar alguns indicadores.")
+    options.append("Imprimir os 20 primeiros registos num novo ficheiro de output mas em formato jason")
 
-name_entity_list()
-#print("")
-#entity_num_elements_list()
-#print("")
-#dist_users_level()
-#name_entity_group()
+    while inputfromuser != '0':
+        print("***Selecione Opção***")
+        for i in range(len(options)):
+            print(f"{i+1} {options[i]}")
+        print("0 Sair.")
 
+        inputfromuser = input(">> ")
+        while int(inputfromuser) > len(options) or int(inputfromuser) < 0:
+            print("Opçao Invalida")
+            inputfromuser = input(">> ")
+
+        if inputfromuser == '0':
+            continue
+        elif inputfromuser == '1':
+            name_entity_list()
+        elif inputfromuser == '2':
+            entity_num_elements_list()
+        elif inputfromuser == '3':
+            dist_users_level()
+        elif inputfromuser == '4':
+            name_entity_group()
+        elif inputfromuser == '5':
+            indicators()
+        else:
+            json_20()
+
+        input("\nPressione Enter ")
+        cls()
+
+
+
+menu()
