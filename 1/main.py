b055f4f47a2e56cd38c40727abbe8bb612bb454c
zoisper <zoisper@gmail.com>
2021-10-30 18:56:25 +0100

    wip

--- 1/main.py
+++ 1/main.py
@@ -1,4 +1,5 @@
 import re
+import unidecode
 
 '''Construa agora um ou vários programas
 para processar o texto ' clav-users.txt ' em que campos de informação
@@ -14,11 +15,11 @@ text = fp.readlines()
 def name_entity_list():
     result = []
     for line in text:
-        name =  re.match(r'(^\w+\s*(\w+\s*)*)',line)
-        entity = re.search(r'ent_\w*',line)
-        result.append((name.group()[:-1], entity.group()))
+        name =  re.match(r'(^\w+\s*(\w+\s*)*\b)',line).group()
+        entity = re.search(r'ent_\w*',line).group()
+        result.append((name, entity))
 
-    result.sort()
+    result.sort(key=lambda x: unidecode.unidecode(x[0].casefold()))
     print("*** Utilizadores - Entidades ***")
     for r in result:
         print(r[0], "-", r[1])
@@ -56,8 +57,34 @@ def dist_users_level():
     result = list(levels.items())
     result.sort()
     total_users = sum(x[1] for x in result )
+    print("*** Nivel de Acesso - Distribuição  ***")
     for r in result:
-        print(f"Nivel:{r[0]} - {round((r[1]/total_users)*100,2)}%" )
+        print(f"{r[0]} - {round((r[1]/total_users)*100,2)}%" )
+
+#Produz uma listagem dos utilizadores, agrupados por entidade, ordenada primeiro pela entidade e dentro desta
+#pelo nome;
+
+def name_entity_group():
+    result = {}
+    for line in text:
+        name = re.match(r'(^\w+\s*(\w+\s*)*\b)', line).group()
+        entity = re.search(r'ent_\w*', line).group()
+        if entity not in result:
+            result[entity] = [name]
+        else:
+            result[entity].append(name)
+
+    entities = list(result.keys())
+    entities.sort()
+    print("*** Utilizadores agrupados por entidade  ***")
+    for entity in entities:
+        print(f"{entity}:")
+        result[entity].sort(key=lambda x: unidecode.unidecode(x.casefold()))
+        for user in result[entity]:
+            print("*",user)
+        print("")
+
+
 
 
 
@@ -68,5 +95,5 @@ def dist_users_level():
 #entity_num_elements_list()
 #print("")
 #dist_users_level()
-
+#name_entity_group()
 
