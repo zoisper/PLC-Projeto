5bc4f27c7f6ad9945c8486937a2ab2acf5284c07
zoisper <zoisper@gmail.com>
2022-01-04 20:40:07 +0000

    wip

--- 2/parser.py
+++ 2/parser.py
@@ -9,12 +9,70 @@ precedence = (
 )
 
 
+def var_new(v):
+	if v[0] in parser.tab_id or v[1] == 0 or v[2] == 0:
+		return -1
+	else:
+		parser.tab_id[v[0]] = (parser.prox_address,v[1],v[2],v[3])
+		parser.prox_address += v[1]*v[2]
+		return 0
+
+def var_address_base(v):
+	if v in parser.tab_id:
+		return parser.tab_id[v][0]
+	else:
+		return -1
+
+def var_address_colum_line(v,colum,line):
+	if var_adress_exists(v,colum,line):
+		return var_address_base(v) + var_num_colums(v)*line + colum 
+	else:
+		return -1
+
+def var_num_colums(v):
+	if v in parser.tab_id:
+		return parser.tab_id[v][1]
+	else:
+		return -1
+
+
+def var_num_lines(v):
+	if v in parser.tab_id:
+		return parser.tab_id[v][2]
+	else:
+		return -1
+
+def var_size(v):
+	if v in parser.tab_id:
+		return parser.tab_id[v][1] * parser.tab_id[v][2]
+	else:
+		return -1
+
+
+def var_type(v):
+	if v in parser.tab_id:
+		return parser.tab_id[v][3]
+	else:
+		return -1
+
+
+def var_adress_exists(name,colum,line):
+	if not name in parser.tab_id:
+		return False
+	elif colum >= var_num_colums(name) or line >= var_num_lines(name):
+		return False
+	else:
+		return True
+
+
+
 def p_program(p):
 	"""
 	program : MAIN LCURLY body RCURLY
 	"""
 	fp.write(p[3])
 	print(p[3])
+
 
 def p_body(p):
 	""" 
@@ -35,66 +93,62 @@ def p_declarations(p):
 	p[0] = p[1] + p[2]
 
 
-def p_declaration_int(p):
+def p_declaration_single(p):
 	"""
-	declaration : INT VAR SEMICOLON 
+	declaration : type VAR SEMICOLON 
 	"""
-	parser.tab_id[p[2]] = ('int',parser.prox_add,1)
-	parser.prox_add +=1
-	p[0] = 'PUSHI 0\n'
 
-
-
-def p_declaration_float(p):
-	"""
-	declaration : FLOAT VAR SEMICOLON 
-	"""
-	parser.tab_id[p[2]] = ('float',parser.prox_add,1)
-	parser.prox_add += 1
-	p[0] = 'PUSHI 0\n'
-
-
-
-def p_declaration_array_int(p):
-	"""
-	declaration : INT LBRACKET NUM RBRACKET VAR SEMICOLON 
-
-	"""
-	parser.tab_id[p[5]] = ('int',parser.prox_add,p[3])
-	parser.prox_add += p[3]
-	p[0] = f'PUSHN {p[3]}\n'
-	
-
-def p_declaration_biarray_int(p):
-	"""
-	declaration : INT LBRACKET NUM RBRACKET LBRACKET NUM RBRACKET VAR SEMICOLON 
-
-	"""
-	parser.tab_id[p[8]] = ('int',parser.prox_add,(p[3],p[6]))
-	parser.prox_add += p[3]*p[6]
-	p[0] = f'PUSHN {p[3]*p[6]}\n'
-
-
-def p_declaration_array_float(p):
-	"""
-	declaration : FLOAT LBRACKET NUM RBRACKET VAR SEMICOLON 
-	"""
-	parser.tab_id[p[5]] = ('float',parser.prox_add,p[3])
-	parser.prox_add += p[3]
-	p[0] = f'PUSHN {p[3]}\n'
-	
-
-def p_declaration_biarray_float(p):
-	"""
-	declaration : FLOAT LBRACKET NUM RBRACKET LBRACKET NUM RBRACKET VAR SEMICOLON 
-
-	"""
-	parser.tab_id[p[8]] = ('float',parser.prox_add,(p[3],p[6]))
-	parser.prox_add += p[3]*p[6]
-	p[0] = f'PUSHN {p[3]*p[6]}\n'
+	status = var_new((p[2],1,1,p[1]))
+	if status == -1:
+		p[0] = f'ERR \"declaração da variavel {p[2]}\"\n'
+	else:
+		p[0] = 'PUSHI 0\n'
 	
 
 
+
+
+
+
+def p_declaration_array(p):
+	"""
+	declaration : type LBRACKET NUM RBRACKET VAR SEMICOLON 
+
+	"""
+	status = var_new((p[5],p[3],1,p[1]))
+	if status == -1:
+		p[0] = f'ERR \"declaração da variavel {p[2]}\"\n'
+	else:
+		p[0] = f'PUSHN {var_size(p[5])}\n'
+
+
+	
+
+def p_declaration_biarray(p):
+	"""
+	declaration : type LBRACKET NUM RBRACKET LBRACKET NUM RBRACKET VAR SEMICOLON 
+
+	"""
+	status = var_new((p[8],p[6],p[3],p[1]))
+	if status == -1:
+		p[0] = f'ERR \"declaração da variavel {p[2]}\"\n'
+	else:
+		p[0] = f'PUSHN {var_size(p[8])}\n'
+
+
+
+
+def p_type_int(p):
+	"""
+	type : INT
+	"""
+	p[0] = f'{p[1]}'
+
+def p_type_float(p):
+	"""
+	type : FLOAT
+	"""
+	p[0] = f'{p[1]}'
 
 
 
@@ -103,34 +157,21 @@ def p_variable_single(p):
 	"""
 	variable : VAR
 	"""
-	p[0] = (parser.tab_id[p[1]][0],parser.tab_id[p[1]][1])
+	p[0] = (p[1],var_address_base(p[1]),var_type(p[1]))
 	
 
 def p_variable_index_num(p):
 	"""
 	variable : VAR LBRACKET NUM RBRACKET
 	"""
-	p[0] = (parser.tab_id[p[1]][0],parser.tab_id[p[1]][1] + p[3])
+	p[0] = (p[1],var_address_colum_line(p[1],p[3],0),var_type(p[1]))
+
 
 def p_variable_index_index_num(p):
 	"""
 	variable : VAR LBRACKET NUM RBRACKET LBRACKET NUM RBRACKET
 	"""
-	p[0] = (parser.tab_id[p[1]][0],parser.tab_id[p[1]][1] + p[3]*parser.tab_id[p[1]][2][1] + p[6])
-
-
-#def p_variable_index_var(p):
-#	"""
-#	variable : VAR LBRACKET VAR RBRACKET
-#	"""
-#	p[0] = (parser.tab_id[p[1]][0],parser.tab_id[p[1]][1] + p[3])
-
-#def p_variable_index_index_var(p):
-#	"""
-#	variable : VAR LBRACKET VAR RBRACKET LBRACKET VAR RBRACKET
-#	"""
-#	p[0] = (parser.tab_id[p[1]][0],parser.tab_id[p[1]][1] + p[3]*parser.tab_id[p[1]][2][1] + p[6])
-#
+	p[0] = (p[1],var_address_colum_line(p[1],p[6],p[3]),var_type(p[1]))
 
 
 
@@ -154,10 +195,21 @@ def p_instruction_atribution_expression(p):
 	"""
 	instruction : variable EQUAL expression SEMICOLON
 	"""
-	if isinstance (p[1],tuple):
+	if p[1][1] == -1:
+		p[0] = f'ERR \"variavel {p[1][0]}\" não existe \n'
+	else:
 		p[0] = p[3] + f'STOREG {p[1][1]}\n'
-	#else:
-	#	p[0] = 
+
+
+def p_instruction_atribution_condition(p):
+	"""
+	instruction : variable EQUAL condition SEMICOLON
+	"""
+	if p[1][1] == -1:
+		p[0] = f'ERR \"variavel {p[1][0]}\" não existe \n'
+	else:
+		p[0] = p[3] + f'STOREG {p[1][1]}\n'
+
 	
 
 
@@ -165,7 +217,12 @@ def p_expression_var(p):
 	"""
 	expression : variable
 	"""
-	p[0] = f'PUSHG {p[1][1]}\n'
+	if p[1][1] == -1:
+		p[0] = f'ERR \"variavel {p[1][0]}\" não existe \n'
+	else:
+		p[0] = f'PUSHG {p[1][1]}\n'
+
+
 
 
 def p_expression_num(p):
@@ -227,7 +284,7 @@ def p_instruction_cicle(p):
 	"""
 	instruction : WHILE LPAREN condition RPAREN LCURLY instructions RCURLY
 	"""
-	p[0] = f'B{parser.labels}:\n' + p[3] + f'E{parser.labels}\n' + p[6] + f'JUMP B{parser.labels}\n' + f'E{parser.labels}:\n' 
+	p[0] = f'B{parser.labels}:\n' + p[3] + 'JZ ' + f'E{parser.labels}\n' + p[6] + f'JUMP B{parser.labels}\n' + f'E{parser.labels}:\n' 
 	parser.labels +=1
 
 
@@ -235,7 +292,7 @@ def p_instruction_conditional(p):
 	"""
 	instruction : IF LPAREN condition RPAREN LCURLY instructions RCURLY
 	"""
-	p[0] = p[3] + f'E{parser.labels}\n' + p[6] + f'E{parser.labels}:\n' 
+	p[0] = p[3] + 'JZ '+ f'E{parser.labels}\n' + p[6] + f'E{parser.labels}:\n' 
 	parser.labels +=1
 	
 
@@ -244,40 +301,38 @@ def p_condition_expression_eqeq_expression(p):
 	"""
 	condition : expression EQEQ expression
 	"""
-	p[0] = p[1] + p[3] + 'EQUAL\nJZ '
+	p[0] = p[1] + p[3] + 'EQUAL\n'
 
 
 def p_condition_expression_diff_expression(p):
 	"""
 	condition : expression DIFF expression
 	"""
-	p[0] = p[1] + p[3] + 'SUB\nJZ '
+	p[0] = p[1] + p[3] + 'SUB\n'
 
 def p_condition_expression_greater_expression(p):
 	"""
 	condition : expression GREATER expression
 	"""
-	p[0] = p[1] + p[3] + 'SUP\nJZ '
+	p[0] = p[1] + p[3] + 'SUP\n'
 
 def p_condition_expression_lesser_expression(p):
 	"""
 	condition : expression LESSER expression
 	"""
-	p[0] = p[1] + p[3] + 'INF\nJZ '
+	p[0] = p[1] + p[3] + 'INF\n'
 
 def p_condition_expression_greateq_expression(p):
 	"""
 	condition : expression GREATEQ expression
 	"""
-	p[0] = p[1] + p[3] + 'SUPEQ\nJZ '
+	p[0] = p[1] + p[3] + 'SUPEQ\n'
 
 def p_condition_expression_lesseq_expression(p):
 	"""
 	condition : expression LESSEQ expression
 	"""
-	p[0] = p[1] + p[3] + 'INFEQ\nJZ '
-
-
+	p[0] = p[1] + p[3] + 'INFEQ\n'
 
 
 
@@ -291,17 +346,27 @@ def p_condition_var(p):
 	"""
 	condition : variable
 	"""
-	p[0] = f'PUSHG {p[1][1]}\nJZ '
+
+	if p[1][1] == -1:
+		p[0] = f'ERR \"variavel {p[1][0]}\" não existe \n'
+	else:
+		p[0] = f'PUSHG {p[1][1]}\nJZ '
+
+
+
 
 def p_instruction_input(p):
 	"""
 	instruction : INPUT LPAREN variable RPAREN SEMICOLON
 	"""
-	p[0] = 'READ\n'
-	if p[3][0] == 'int':
-		p[0] += 'ATOI\n'
+	if p[3][1] == -1:
+		p[0] = f'ERR \"variavel {p[3][0]}\" não existe \n'
 	else:
-		p[0] += 'ATOF\n'
+		p[0] = 'READ\n'
+		if p[3][2] == 'int':
+			p[0] += 'ATOI\n'
+		else:
+			p[0] += 'ATOF\n'
 
 	p[0] += f'STOREG {p[3][1]}\n' 
 
@@ -310,26 +375,30 @@ def p_instruction_print_var(p):
 	"""
 	instruction : PRINT LPAREN variable RPAREN SEMICOLON
 	"""
-	p[0] = f'PUSHG {p[3][1]}\n'
-
-	if p[3][0] == 'int':
-		p[0] += 'WRITEI\n'
+	if p[3][1] == -1:
+		p[0] = f'ERR \"variavel {p[3][0]}\" não existe \n'
 	else:
-		p[0] += 'WRITEF\n'
+		p[0] = f'PUSHG {p[3][1]}\n'
+		if p[3][2] == 'int':
+			p[0] += 'WRITEI\n'
+		else:
+			p[0] += 'WRITEF\n'
 
 
 def p_instruction_println_var(p):
 	"""
 	instruction : PRINTLN LPAREN variable RPAREN SEMICOLON
 	"""
-	p[0] = f'PUSHG {p[3][1]}\n'
-
-	if p[3][0] == 'int':
-		p[0] += 'WRITEI\n'
+	if p[3][1] == -1:
+		p[0] = f'ERR \"variavel {p[3][0]}\" não existe \n'
 	else:
-		p[0] += 'WRITEF\n'
+		p[0] = f'PUSHG {p[3][1]}\n'
+		if p[3][2] == 'int':
+			p[0] += 'WRITEI\n'
+		else:
+			p[0] += 'WRITEF\n'
 
-	p[0] += 'PUSHS\"\\n\"\nWRITES\n'
+		p[0] += 'PUSHS\"\\n\"\nWRITES\n'
 
 
 
@@ -360,9 +429,11 @@ def p_error(p):
 parser = yacc.yacc()
 parser.success = True
 
-parser.prox_add = 0
+parser.prox_address = 0
 parser.tab_id = {}
 parser.labels = 0
+
+
 
 
 if len(sys.argv)!=2 and len(sys.argv)!=3:
@@ -389,6 +460,7 @@ else:
 fp = open(file_output,"w")
 
 parser.parse("".join(source))
+
 
 
 if parser.success:
